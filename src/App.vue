5f05506a856dc3b31b4aa85f63760b4c533879f5
naglec13 <naglec13@gmail.com>
2020-08-04 14:18:54 +0300

    added new component TodoList to the App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Todo Application</h1>
+    <hr/>
+    <TodoList/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import TodoList from '@/components/TodoList'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  TodoList
   }
 }
 </script>
