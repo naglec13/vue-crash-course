9ffe4669a096ca829fe67ac1b5b87ec23871dfa6
naglec13 <naglec13@gmail.com>
2020-08-04 14:27:57 +0300

    added component TodoItem to TodoList

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,5 @@
+<template>
+  <li>
+    Todo Item 1
+  </li>
+</template>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,7 +1,16 @@
 <template>
   <div>
     <ul>
-      <li>todo 1</li>
+      <TodoItem/>
     </ul>
   </div>
 </template>
+
+<script>
+import TodoItem from '@/components/TodoItem'
+export default{
+  components:{
+     TodoItem
+  }
+}
+</script>
