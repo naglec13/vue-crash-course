f35d1ffa0269f685ce21eb76cc13aeaee987c510
naglec13 <naglec13@gmail.com>
2020-08-06 23:59:13 +0300

    added select section for filtering array of todos

--- src/views/Todos.vue
+++ src/views/Todos.vue
@@ -3,6 +3,12 @@
     <h1>Todo Application</h1>
     <router-link to="/">Home</router-link>
     <hr/>
+    <select v-model="filtering">
+      <option value="all">all</option>
+      <option value="completed">completed</option>
+      <option value="not-completed">not-completed</option>
+    </select>
+    <hr/>
     <AddTodo
         @add-todo="addTodo"
     />
@@ -11,8 +17,8 @@
         v-if="loadingTodos"
     />
     <TodoList
-        v-else-if="todos.length"
-        v-bind:todos="todos"
+        v-else-if="filteredTodos.length"
+        v-bind:todos="filteredTodos"
         @remove-todo="removeTodo"
     />
     <p v-else>No Todos!</p>
@@ -36,7 +42,8 @@ export default {
         // {id: 3, title: 'Buy some sugar', completed: false},
         // {id: 4, title: 'Buy some whisky', completed: false}
       ],
-      loadingTodos: true
+      loadingTodos: true,
+      filtering: 'all'
     }
   },
   mounted() {
@@ -44,10 +51,28 @@ export default {
         .then(response => response.json())
         .then(json => {
           setTimeout(() => {
-            this.todos = json,
-                this.loadingTodos = false
+            this.todos = json
+            this.loadingTodos = false
           }, 1500)
         })
+  },
+  // watch: {
+  //   filter(value){
+  //     console.log(value)
+  //   }
+  // },
+  computed:{
+    filteredTodos(){
+        if (this.filtering === 'all') {
+          return this.todos
+        }
+        if (this.filtering === 'completed') {
+          return  this.todos.filter(t => t.completed)
+        }
+        if (this.filtering === 'non-completed') {
+          return  this.todos.filter(t => !t.completed)
+        }
+    }
   },
   methods: {
     removeTodo(id) {
